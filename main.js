4d0f0a287f38360dd06633b4264d7e6022e9a458
dillius <dillius@dillius.com>
2015-07-30 20:46:58 -0400

    Initial code.

---
+++ main.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  console.log("test");
+  findTimezone();
+});
+
+const targetHour = 17;
+var cities = new Array();
+cities[-12] = "Pacific Ocean";
+cities[-11] = "Pago Pago, American Samoa"; 
+cities[-10] = "Honolulu, USA";
+cities[-9] = "Anchorage, USA"; 
+cities[-8] = "Los Angeles, USA";
+cities[-7] = "Phoenix, USA"; 
+cities[-6] = "Mexico City, Mexico"; 
+cities[-5] = "New York City, USA"; 
+cities[-4] = "Santiago, Chile"; 
+cities[-3] = "Sao Paulo, Brazil"; 
+cities[-2] = "Atlantic Ocean"; 
+cities[-1] = "Praia, Cape Verde"; 
+cities[0] = "London, England"; 
+cities[1] = "Lagos, Nigeria"; 
+cities[2] = "Cairo, Egypt"; 
+cities[3] = "Moscow, Russia"; 
+cities[4] = "Baku, Azerbaijan"; 
+cities[5] = "Karachi, Pakistan"; 
+cities[6] = "Dhaka, Bangladesh"; 
+cities[7] = "Jakarta, Indonesia"; 
+cities[8] = "Guangzhou, China"; 
+cities[9] = "Tokyo, Japan"; 
+cities[10] = "Sydney, Australia"; 
+cities[11] = "Noumea, New Caledonia";
+cities[12] = "Auckland, New Zealand"; 
+
+function getOffset() {
+  var utcHour = new Date().getUTCHours();
+  if(utcHour == 0) utcHour = 24;
+  return targetHour - utcHour;
+}
+
+function getCity(offset) {
+  return cities[offset];
+}
+
+function getTime(offset) {
+  var d = new Date();
+
+  var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+
+  var nd = new Date(utc + (3600000*offset));
+
+  return nd;
+}
+
+function findTimezone() {
+  var cityContainer = document.getElementById('cityContainer');
+  var timeContainer = document.getElementById('timeContainer');
+  var offset = getOffset();
+  cityContainer.innerHTML = getCity(offset);
+  timeContainer.innerHTML = getTime(offset).toLocaleTimeString();
+};
